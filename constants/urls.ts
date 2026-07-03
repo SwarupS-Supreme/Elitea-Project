98908071638beda823e4ef0bd4f3c8519bdf0a9c
SwarupS-Supreme <117153400+SwarupS-Supreme@users.noreply.github.com>
2026-07-03 17:27:05 +0530

    feat: add Playwright POM navigation scaffolding

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,3 +1,9 @@
 export const AppUrls = {
-  home: 'https://www.epam.com/',
+  epamHome: 'https://www.epam.com/',
+} as const;
+
+export const PageTexts = {
+  servicesMenu: 'Services',
+  exploreOurClientWork: 'Explore Our Client Work',
+  clientWorkHeading: 'Client Work',
 } as const;
