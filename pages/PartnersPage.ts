201c47ba2ce3c77cd077949976986355e1992604
SwarupS-Supreme <117153400+SwarupS-Supreme@users.noreply.github.com>
2026-07-07 13:58:38 +0530

    Update pages/PartnersPage.ts

--- pages/PartnersPage.ts
+++ pages/PartnersPage.ts
@@ -1,12 +1,11 @@
 import { type Locator, type Page } from '@playwright/test';
-import { PageTexts } from '../constants/urls.js';
 
 export class PartnersPage {
   readonly heading: Locator;
 
   constructor(private readonly page: Page) {
     this.heading = this.page.getByRole('heading', {
-      name: PageTexts.partnersHeading,
+      name: 'Partners',
       exact: true,
     });
   }
