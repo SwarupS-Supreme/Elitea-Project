b61d416e17d0354eb2eda233b0222253833cdd9e
SwarupS-Supreme <117153400+SwarupS-Supreme@users.noreply.github.com>
2026-07-09 16:47:17 +0530

    Create pages/epamHomePage.ts

---
+++ pages/epamHomePage.ts
@@ -0,0 +1,35 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class EpamHomePage {
+  readonly page: Page;
+  readonly servicesMenu: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.servicesMenu = page.getByRole('link', { name: 'Services' });
+  }
+
+  async open() {
+    await this.page.goto('/');
+    await this.page.waitForLoadState('domcontentloaded');
+  }
+
+  async openServicesMenu() {
+    await this.servicesMenu.hover();
+    await this.servicesMenu.click();
+  }
+
+  async openClientWork() {
+    await this.openServicesMenu();
+    await this.page.getByRole('link', { name: 'Client Work' }).click();
+  }
+
+  async openPartners() {
+    await this.openServicesMenu();
+    await this.page.getByRole('link', { name: 'Partners' }).click();
+  }
+
+  async expectTextVisible(text: string) {
+    await expect(this.page.getByText(text, { exact: true })).toBeVisible();
+  }
+}
